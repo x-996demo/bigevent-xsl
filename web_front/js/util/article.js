47cbdb3a0433c8471d7f9a677bc90fc0e460c22a
xiesailong <x976403038@163.com>
2020-02-09 13:15:39 +0800

    Es6箭头函数改造

--- web_front/js/util/article.js
+++ web_front/js/util/article.js
@@ -1,47 +1,68 @@
 // 把所有与文章相关的操作写在这里
 
-var article = {
-    /**
-     * 获取主页中5条焦点图新闻
-     */
-    getFouceFive:function(){
-        // 因为只要5条数据，所以直接设置只获取5条
-        return $.get(APILIST.article_get,{perpage: 5})
+// var article = {
+//     /**
+//      * 获取主页中5条焦点图新闻
+//      */
+//     getFouceFive:function(){
+//         // 因为只要5条数据，所以直接设置只获取5条
+//         return $.get(APILIST.article_get,{perpage: 5})
+//     },
+//     /**
+//      * 根据id去获取文章的详情
+//      * @param {Number}} id 
+//      */
+//     getById:function(id){
+//         return $.get(APILIST.article_get,{'id':id})
+//     },
+//     /**
+//      * 获取文章排行
+//      * @param {*} type  文章类别编号 
+//      * 如果不传则在全部的类别中进行排行。
+//      */
+//     getRank:function(type){
+//         return $.get(APILIST.article_rank,{'type':type})
+//     },
+//     /**
+//      * 获取最新的文章
+//      */
+//     getLastest:function(){
+//         return $.get(APILIST.article_last)
+//     },
+//     /***
+//      * 在列表页中，用来获取文章列表数据
+//      * type: 文章类型编号
+//      * page: 当前第几页
+//      */
+//     getList:function(type,page) {
+//         return $.get(APILIST.article_get,{'type':type,'page':page})
+//     },
+//     /**
+//      * 获取文章详情
+//      * @param {*} id 文章编号
+//      */
+//     getDetail:function(id){
+//         return $.get(APILIST.article_detail,{'id':id})
+//     }
+// }
+const article = {
+    // 获取
+    getList: (type,page) => $.get(APILIST.article_get, {type,page}),
+
+    // 热门排行
+    rank:(type)=>{
+        return $.get(APILIST.article_rank,{type})
     },
+
     /**
-     * 根据id去获取文章的详情
-     * @param {Number}} id 
+     * 5张焦点图 
      */
-    getById:function(id){
-        return $.get(APILIST.article_get,{'id':id})
-    },
-    /**
-     * 获取文章排行
-     * @param {*} type  文章类别编号 
-     * 如果不传则在全部的类别中进行排行。
-     */
-    getRank:function(type){
-        return $.get(APILIST.article_rank,{'type':type})
-    },
-    /**
-     * 获取最新的文章
-     */
-    getLastest:function(){
-        return $.get(APILIST.article_last)
-    },
-    /***
-     * 在列表页中，用来获取文章列表数据
-     * type: 文章类型编号
-     * page: 当前第几页
-     */
-    getList:function(type,page) {
-        return $.get(APILIST.article_get,{'type':type,'page':page})
-    },
-    /**
-     * 获取文章详情
-     * @param {*} id 文章编号
-     */
-    getDetail:function(id){
-        return $.get(APILIST.article_detail,{'id':id})
-    }
+    hotImg: perpage => $.get(APILIST.article_search,{'perpage':5}),
+
+    // 最新资讯
+    lastest: () => $.get(APILIST.article_lastest),
+
+    // 文章详情
+    getById: id => $.get(APILIST.article_info,{id})
+
 }
